cb17790bc9f4f254bff9e9ad6dca96d8879983b8
seonjiwon <tommys915@gmail.com>
2024-10-30 17:16:17 +0900

    reissue controller 구현

--- src/main/java/inyro/SMSinmungBE/repository/RefreshRepository.java
+++ src/main/java/inyro/SMSinmungBE/repository/RefreshRepository.java
@@ -2,7 +2,11 @@ package inyro.SMSinmungBE.repository;
 
 import inyro.SMSinmungBE.config.security.jwt.RefreshEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface RefreshRepository extends JpaRepository<RefreshEntity, Long> {
+  Boolean existsByRefresh(String refresh);
 
+  @Transactional
+  void deleteByRefresh(String refresh);
 }
