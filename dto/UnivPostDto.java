a4824175b021b30e82b80494e1d26bd3d35eae5a
오준영 <89961166+ojy0903@users.noreply.github.com>
2024-11-05 20:31:14 +0900

    Add files via upload

---
+++ dto/UnivPostDto.java
@@ -0,0 +1,20 @@
+package inyro.demo.dto;
+import inyro.demo.model.Member;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UnivPostDto {
+
+    private String author;
+    private String password;
+    private String title;
+    private String contents;
+    private String category;
+    private int good;
+    private int view;
+    private Member member;
+}
