4fb747ab7d6d333dbd98522e5df8d7e51b3e8f9c
박진영 <am030813@naver.com>
2024-11-13 22:20:43 +0900

    Commit Changes

--- SmSinmungoApplication.java
+++ SmSinmungoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmSinmungoApplication {
 
 	public static void main(String[] args) {
+		// Spring Boot 애플리케이션 시작
 		SpringApplication.run(SmSinmungoApplication.class, args);
 	}
-
 }
