4a49ba5b608c17cb52a261a3dad8f0cd6742cce6
박진영 <am030813@naver.com>
2024-11-20 20:59:04 +0900

    FEAT: Member 엔티티와 다대다 관계 추가

--- src/main/java/com/smsinmungo/model/UnivPost.java
+++ src/main/java/com/smsinmungo/model/UnivPost.java
@@ -1,25 +1,26 @@
 package com.smsinmungo.model;
 
-import com.smsinmungo.dto.UnivPostDto;
+import com.smsinmungo.domain.Member;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Builder
 @AllArgsConstructor
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class UnivPost extends Timestamp{
+public class UnivPost{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
     private String author;
-
-    @Column(nullable = false)
-    private String password;
 
     @Column(nullable = false)
     private String title;
@@ -30,27 +31,25 @@ public class UnivPost extends Timestamp{
     @Column(nullable = false)
     private String category;
 
-    @Column(nullable = false)
+    @Column
     private int view;
 
-    @Column(nullable = false)
+    @Column
     private int good;
 
-    @Column(nullable = false)
+    @Column
     private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column
+    private LocalDateTime createdAt;
+    @Column
+    private LocalDateTime modifiedAt;
 
     public void like() {
         this.good++;
-    }
-
-    public UnivPost(UnivPostDto univPostDto) {
-        this.author = univPostDto.getAuthor();
-        this.password = univPostDto.getPassword();
-        this.title = univPostDto.getTitle();
-        this.contents = univPostDto.getContents();
-        this.category = univPostDto.getCategory();
-        this.view = 0;
-        this.good = 0;
-        this.status = "NEW";
     }
 }
