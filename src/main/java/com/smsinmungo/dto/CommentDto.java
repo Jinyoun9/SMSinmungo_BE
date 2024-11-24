9ecad4465a678d571972c9f0534322a71b69c52c
오준영 <89961166+ojy0903@users.noreply.github.com>
2024-11-24 15:28:10 +0900

    post 객체 추가

--- src/main/java/com/smsinmungo/dto/CommentDto.java
+++ src/main/java/com/smsinmungo/dto/CommentDto.java
@@ -2,6 +2,7 @@ package com.smsinmungo.dto;
 
 import com.smsinmungo.domain.Member;
 
+import com.smsinmungo.model.Post;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,5 +13,6 @@ import lombok.NoArgsConstructor;
 public class CommentDto {
 
     private Member member;
+    private Post post; //댓글이 달린 게시물 정보 추가
     private String content;
 }
